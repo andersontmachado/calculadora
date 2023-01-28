32de6c529370e74399765d5ebf14979b3f7c7d37
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2023-01-27 23:15:22 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,34 @@
-function calculatetip(event) {
+function calculateTip(event) {
     event.preventDefault();
+    let bill = document.getElementById("bill").value;
+    let servicesQualid = document.getElementById("quali").value;
+    let quantipeople = document.getElementById("people").value;
+
+    if(bill == " " | servicesQualid == 0){
+        //se a conta estiver vazia ou estiver zerada,crie esse alerta
+        alert("Por favor,preencha os valores")
+        return;
+    }
+    if(quantipeople == 0 | quantipeople <=1){
+        quantipeople=1;
+        document.getElementById("each").style.display="none";
+}
+else{
+    document.getElementById("each").style.display="block";
+    //block faz aparecer
+
+}
+let total= (bill * quali) /people;
+total=total.toFixed(2);
+document.getElementById("Tip").innerHTML = total;
+document.getElementById("totaltip").style.display="block";
+
+
+
 }
 
 document.getElementById("totaltip").style.display="none";
 document.getElementById("each").style.display="none";
 //  desaparece todo o contéudo da totaltip e each
 
-document.getElementById("formtips").addEventListener('submit').calculatetip;
+document.getElementById("formtips").addEventListener("submit",calculateTip);
