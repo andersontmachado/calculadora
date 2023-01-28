99c9ccab2023a8487bfe77fa493731647d260c44
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2023-01-27 22:28:44 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function calculatetip(event) {
+    event.preventDefault();
+}
+
+document.getElementById("totaltip").style.display="none";
+document.getElementById("each").style.display="none";
+//  desaparece todo o contéudo da totaltip e each
+
+document.getElementById("formtips").addEventListener('submit').calculatetip;
